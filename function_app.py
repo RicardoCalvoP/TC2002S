3a3172a386c00ff72a6fcf3d785b0fbfa5090e3e
RicardoCalvoP <a01028889@tec.mx>
2024-10-22 12:02:12 -0600

    deleted api key

--- function_app.py
+++ function_app.py
@@ -1,5 +1,8 @@
 import azure.functions as func
 import logging
+from openai import OpenAI
+secret_key = ""
+
 
 app = func.FunctionApp(http_auth_level=func.AuthLevel.ANONYMOUS)
 
@@ -23,3 +26,45 @@ def firstFunctionAPI(req: func.HttpRequest) -> func.HttpResponse:
              "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
              status_code=200
         )
+
+@app.route(route="completionAPI", auth_level=func.AuthLevel.ANONYMOUS)
+def completionAPI(req: func.HttpRequest) -> func.HttpResponse:
+    logging.info('Python HTTP trigger function processed a request.')
+
+    client = OpenAI(
+        api_key = secret_key
+    )
+
+    req_body = req.get_json()
+
+    completion = client.chat.completions.create(
+        model="gpt-3.5-turbo",
+        messages=[
+            {  "role": "system","content": "You are a helpfull asistant"},
+            {  "role": "user","content": req_body["prompt"]},
+        ],
+        max_tokens = 500,
+        temperature = 1,
+    )
+    return func.HttpResponse(completion.choices[0].message.content, status_code=200)
+
+
+@app.route(route="ImageGeneratorAPI", auth_level=func.AuthLevel.ANONYMOUS)
+def ImageGeneratorAPI(req: func.HttpRequest) -> func.HttpResponse:
+    logging.info('Python HTTP trigger function processed a request.')
+
+    client = OpenAI(
+        api_key = secret_key
+    )
+
+    req_body = req.get_json()
+
+    response = client.images.generate(
+        model="dall-e-3",
+        prompt= req_body["prompt"],
+        size="1024x1024",
+        quality="standard",
+        n=1,
+    )
+
+    return (response.data[0].url)
